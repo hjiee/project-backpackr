60b65abf2296d3bd3177babb3ee2519e7d61a2bd
hjiee <jin33032@gmail.com>
2019-12-05 17:47:58 +0900

    ADD [ Detail View 추가 ]

--- app/src/main/java/com/hjiee/appproject/view/DetailActivity.kt
+++ app/src/main/java/com/hjiee/appproject/view/DetailActivity.kt
@@ -1,16 +1,65 @@
 package com.hjiee.appproject.view
 
+import android.graphics.Paint
 import android.os.Bundle
+import android.text.TextUtils
+import androidx.databinding.library.baseAdapters.BR
+import androidx.lifecycle.Observer
+import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.slider.Slider
 import com.hjiee.appproject.R
 import com.hjiee.appproject.base.BaseActivity
+import com.hjiee.appproject.base.BaseRecyclerView
+import com.hjiee.appproject.data.remote.network.Body
 import com.hjiee.appproject.databinding.ActivityDetailBinding
+import com.hjiee.appproject.databinding.PagerDetailItemBinding
+import com.hjiee.appproject.util.PageChangedCallbackListener
+import com.hyden.ext.loadUrl
+import com.hyden.util.LogUtil.LogW
+import kotlinx.android.synthetic.main.activity_detail.*
+import org.koin.android.ext.android.inject
 
 class DetailActivity : BaseActivity<ActivityDetailBinding>(R.layout.activity_detail) {
+
+    private val detailViewModel by inject<DetailViewModel>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        intent.getIntExtra("clicked_item",0).let {
+            detailViewModel.loadDetailInfo(it)
+        }
+        initBind()
     }
 
     override fun initBind() {
+        binding.apply {
+            vm = detailViewModel
+            vpThumnail.apply {
+                offscreenPageLimit = 3
+                registerOnPageChangeCallback(PageChangedCallbackListener())
+                adapter = object : BaseRecyclerView.Adapter<String,PagerDetailItemBinding,String>(
+                    layoutId = R.layout.pager_detail_item,
+                    bindingVariableId = BR.imageUrl
+                ) { }
+            }
 
+            fbClose.apply {
+                setOnClickListener { finish() }
+            }
+
+            tvCost.apply {
+                detailViewModel.detailInfo.observe(
+                    this@DetailActivity,
+                    Observer {
+                        when(it.discount_rate.isNullOrEmpty()) {
+                            true -> { /* no operation */ }
+                            false -> {
+                                paintFlags = this.paintFlags.or(Paint.STRIKE_THRU_TEXT_FLAG)
+                                setTextAppearance(R.style.DetailCostTheme)
+                            }
+                        }
+                    }
+                )
+            }
+        }
     }
 }
--- app/src/main/java/com/hjiee/appproject/view/DetailViewModel.kt
+++ app/src/main/java/com/hjiee/appproject/view/DetailViewModel.kt
@@ -1,7 +1,37 @@
 package com.hjiee.appproject.view
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.hjiee.appproject.base.BaseViewModel
+import com.hjiee.appproject.data.remote.network.Body
+import com.hjiee.appproject.data.repository.ProductRepository
+import com.hyden.util.LogUtil
 
-class DetailViewModel : ViewModel() {
+class DetailViewModel(
+    private val productRepository: ProductRepository
+) : BaseViewModel() {
+
+    private val _detailInfo = MutableLiveData<Body>()
+    val detailInfo: LiveData<Body> get() = _detailInfo
+
+    private val _thumbnailList = MutableLiveData<List<String>>()
+    val thumbnailList: LiveData<List<String>> get() = _thumbnailList
+
+    fun loadDetailInfo(
+        id: Int
+    ) {
+        addDisposable(
+            productRepository.detailInfo(
+                id = id,
+                success = { data ->
+                    _detailInfo.value = data[0]
+                    _thumbnailList.value = data[0].thumbnail_list_320?.split("#")
+                },
+                failure = {
+                    LogUtil.LogW(it)
+                })
+        )
+    }
 
 }
